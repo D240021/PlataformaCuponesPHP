94c660f1f0d6a76691164906e2c1146cccdd3e1a
D240021 <danielbarrantesq@gmail.com>
2024-05-27 21:12:31 -0600

    Se agrega estructura basica del backend de PHP y se agrega la clase Empresa.php

---
+++ Backend PHP/Dominio/Empresa.php
@@ -0,0 +1,24 @@
+<?php
+
+class Empresa
+{
+
+    public $nombre;
+    public $direccion;
+    public $cedula;
+    public $fecha;
+    public $correo;
+    public $telefono;
+    public $contrasenia;
+
+}
+
+
+
+
+
+
+
+
+
+
